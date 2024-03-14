60075adff0a9668a60fe98775eee7a97c66f9380
G2Squared <gesket127@gmail.com>
2024-03-14 02:36:38 -0500

    Updated missing seeds

---
+++ seeds/seed.js
@@ -0,0 +1,36 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => { 
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, { // Create the users
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) { 
+    await Blog.create({ // Create the blogs
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const blogs = await Blog.findAll(); // get the blogs
+
+  for (const comment of commentData) {
+    await Comment.create({ // Create the comments
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: blogs[Math.floor(Math.random() * blogs.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
